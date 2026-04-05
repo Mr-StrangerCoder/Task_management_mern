e613cd6bba34d6c572c0c5c036ca8879c7465514
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-04-05 16:41:39 +0530

    create server

---
+++ Task_backed/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+require("dotenv").config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on http://locolhost:${port}`)
+})
